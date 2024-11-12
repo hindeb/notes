7d430ee3941769c2ac92cdb867d50c17475755ec
hindeb <hindbosundal@gmail.com>
2024-11-12 10:22:08 +0300

    Initial commit

--- sst.config.ts
+++ sst.config.ts
@@ -1,19 +1,16 @@
 /// <reference path="./.sst/platform/config.d.ts" />
+//testing
 
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "notes",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
   },
   async run() {
     await import("./infra/storage");
-    const api = await import("./infra/api");
-
-    return {
-      api: api.myApi.url,
-    };
+    await import("./infra/api");
   },
 });
